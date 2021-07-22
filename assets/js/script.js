22e6b25baeff78a8bdc3d366626d64d7e935353f
Bomb Atomically <ieugenelee@gmail.com>
2021-07-22 11:32:56 -0700

    created btn click function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,10 @@
 // https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 
+// vanilla JS - document.get - addEvent, callback
+
+// 1 added jQuery function for searchBtn
+$("#searchBtn").on("click",function(event){
+    event.preventDefault()
+    var userInput = $("#city").val()  // same as .value in JS // create variable userInput and assign id to "#city" (value in text input);
+    console.log(userInput)
+});
