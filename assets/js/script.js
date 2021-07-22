ecbb0f097b2652465894cfaab78bf5f22abfa686
Bomb Atomically <ieugenelee@gmail.com>
2021-07-22 11:54:10 -0700

    created function forecast to pull from city

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,22 @@
-// https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+var apiKey = "ec068ec2b1a3152772ec0cdd7e170469"; // 2 added apiKey 
 
-// vanilla JS - document.get - addEvent, callback
 
-// 1 added jQuery function for searchBtn
+
+
+// 1 added jQuery function for searchBtn // vanilla JS - document.get - addEvent, callback
 $("#searchBtn").on("click",function(event){
     event.preventDefault()
     var userInput = $("#city").val()  // same as .value in JS // create variable userInput and assign id to "#city" (value in text input);
-    console.log(userInput)
+    forecast(userInput) // 3 calling forecast function
 });
+
+function forecast(cityName) {
+    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial` // 2 added https to pull from website, then we included a parament (cityname)
+    $.ajax({ // 4 method to place an API call through jQuery
+        method:"GET",
+        url:queryURL
+    }).then(function(response) { // only responds to the code after when using .then
+        console.log(response)
+    })
+};
+
